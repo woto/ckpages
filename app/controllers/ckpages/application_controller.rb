08130d4cc46f933c3b38a4d41293a00bfe2c26e2
Ruslan Kornev <oganer@gmail.com>
2015-04-27 15:53:20 +0500

    Ckpages name -> login

--- app/controllers/ckpages/application_controller.rb
+++ app/controllers/ckpages/application_controller.rb
@@ -1,5 +1,5 @@
 module Ckpages
   class ApplicationController < ActionController::Base
-    http_basic_authenticate_with name: Ckpages.name, password: Ckpages.password
+    http_basic_authenticate_with name: Ckpages.login, password: Ckpages.password
   end
 end
