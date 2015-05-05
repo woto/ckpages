c6ef8416de8d9537b6a740cf93fe33e92c3c84c1
Ruslan Kornev <oganer@gmail.com>
2015-05-05 10:50:37 +0300

    Исправил подключение middleware static

--- lib/ckpages/engine.rb
+++ lib/ckpages/engine.rb
@@ -2,7 +2,7 @@ module Ckpages
   class Engine < ::Rails::Engine
     isolate_namespace Ckpages
     initializer "static assets" do |app|
-        app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
+        app.middleware.insert_before(::Rack::Sendfile, ::ActionDispatch::Static, "#{root}/public")
     end
   end
 end
