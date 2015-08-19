29de985dff08d979ae4df906bc92560eb762edcf
Ruslan Kornev <oganer@gmail.com>
2015-08-20 04:13:52 +0500

    ... но не сейчас :)

--- db/migrate/20150819230152_add_columns_to_ckpages_uploads.rb
+++ db/migrate/20150819230152_add_columns_to_ckpages_uploads.rb
@@ -1,6 +1,6 @@
 class AddColumnsToCkpagesUploads < ActiveRecord::Migration
   def change
-    add_column :ckpages_uploads, :file, :string
+    rename_column :ckpages_uploads, :image, :file
     add_column :ckpages_uploads, :content_type, :string
     add_column :ckpages_uploads, :file_size, :integer
   end
