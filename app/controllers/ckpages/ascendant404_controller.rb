9474732abd671a3890d183245b6cb3e200302b9e
Ruslan Kornev <oganer@gmail.com>
2015-08-30 18:45:44 +0300

    Убрал exception_notification

--- app/controllers/ckpages/ascendant404_controller.rb
+++ app/controllers/ckpages/ascendant404_controller.rb
@@ -7,17 +7,6 @@ module Ckpages
     private
 
       def set_status_404
-        # TODO Пока так, пока не найду хорошего способа. Пользователь не всегда хочет
-        # чтобы raise exceptions.
-        #
-        # Памятка. Вырезано с yaponama.
-        # Это более не актуально. Необходимо использовать config.exceptions_app ? или
-        # wildcard match (*) как это сейчас в ckpages ? Или config.action_dispatch.rescue_responses ?
-        # http://www.youtube.com/results?search_query=exception+railscasts
-        # rescue_from ActionController::RoutingError, :with => :render_404
-        if Module.const_get "ExceptionNotifier"
-          ExceptionNotifier.notify_exception(Exception.new("page doesn't exists"), :env => request.env, :data => {:message => "page doesn't exists"})
-        end
         response.status = 404
       end
   end
