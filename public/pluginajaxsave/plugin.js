ea8586e2b8b5d0f50a628e2097b3ab55f1ae104e
Ruslan Kornev <oganer@gmail.com>
2016-02-28 03:21:52 +0300

    На скорую руку сделал pluginajaxsave

---
+++ public/pluginajaxsave/plugin.js
@@ -0,0 +1,23 @@
+﻿( function() {
+	var pluginName = 'pluginajaxsave';
+
+	// Register a plugin named "pluginajaxsave".
+	CKEDITOR.plugins.add( pluginName, {
+		icons: 'pluginajaxsave',
+		init: function( editor ) {
+			editor.ui.addButton( 'Save', {
+				label: 'Cохранить',
+				command: pluginName,
+        click: function(editor) {
+          $.ajax({
+            type: "PATCH",
+            url: $(editor.element.$.form).attr('action'),
+            data: {page: {content: editor.getData()}},
+            dataType: 'json'
+          });
+        },
+        toolbar: 'insert'
+			} );
+		}
+	} );
+} )();
